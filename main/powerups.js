690904aec9eba157526e85aae295da4cb4de67ce
Michael Partridge <partridgems@gmail.com>
2015-11-13 20:59:45 -0500

    Tuned shield appearance

--- main/powerups.js
+++ main/powerups.js
@@ -1,11 +1,11 @@
 function addShieldPowerup() {
 
     pMat = new THREE.MeshBasicMaterial({color: 0xffaa00, wireframe: true});
-    spUp = new Physijs.SphereMesh(new THREE.SphereGeometry(5, 50, 50), pMat);
+    spUp = new Physijs.SphereMesh(new THREE.SphereGeometry(3, 10, 10), pMat);
     spUp.position.copy(getBoardPoint());
     spUp.position.y = avatarHeight;
     spUp.name = "ShieldPowerup";
 
     scene.add(spUp);
-    spUp.setAngularVelocity(new THREE.Vector3(0, 3, 0));
+    spUp.setAngularVelocity(new THREE.Vector3(0, 1, 0));
 }
